ced96bbeb1799c5cdb9f9196b917ddabd33aa8e0
Danilo Horta <danilo.horta@pm.me>
2021-08-31 13:22:57 +0100

    Handle damaged files

--- test/read_fasta.c
+++ test/read_fasta.c
@@ -3,11 +3,17 @@
 
 void test_read_empty(void);
 void test_read_mix(void);
+void test_read_damaged1(void);
+void test_read_damaged2(void);
+void test_read_damaged3(void);
 
 int main(void)
 {
     test_read_empty();
     test_read_mix();
+    test_read_damaged1();
+    test_read_damaged2();
+    test_read_damaged3();
     return hope_status();
 }
 
@@ -69,6 +75,76 @@ void test_read_mix(void)
         i++;
     }
     EQ(i, 4);
+    EQ(rc, FAR_ENDFILE);
+
+    fclose(fd);
+}
+
+void test_read_damaged1(void)
+{
+    FILE *fd = fopen(ASSETS "/damaged1.faa", "r");
+    NOTNULL(fd);
+
+    FAR_DECLARE(far, fd);
+    FAR_TGT_DECLARE(tgt, &far);
+
+    unsigned i = 0;
+    enum far_rc rc = FAR_SUCCESS;
+    while (!(rc = far_next_tgt(&far, &tgt)))
+    {
+        i++;
+    }
+    EQ(i, 0);
+    EQ(rc, FAR_PARSEERROR);
+    EQ(far.error, "Parse error: unexpected token: line 1");
+    far_clear_error(&far);
+    EQ(far.error, "");
+
+    fclose(fd);
+}
+
+void test_read_damaged2(void)
+{
+    FILE *fd = fopen(ASSETS "/damaged2.faa", "r");
+    NOTNULL(fd);
+
+    FAR_DECLARE(far, fd);
+    FAR_TGT_DECLARE(tgt, &far);
+
+    unsigned i = 0;
+    enum far_rc rc = FAR_SUCCESS;
+    while (!(rc = far_next_tgt(&far, &tgt)))
+    {
+        i++;
+    }
+    EQ(i, 0);
+    EQ(rc, FAR_PARSEERROR);
+    EQ(far.error, "Parse error: unexpected id: line 2");
+    far_clear_error(&far);
+    EQ(far.error, "");
+
+    fclose(fd);
+}
+
+void test_read_damaged3(void)
+{
+    FILE *fd = fopen(ASSETS "/damaged3.faa", "r");
+    NOTNULL(fd);
+
+    FAR_DECLARE(far, fd);
+    FAR_TGT_DECLARE(tgt, &far);
+
+    unsigned i = 0;
+    enum far_rc rc = FAR_SUCCESS;
+    while (!(rc = far_next_tgt(&far, &tgt)))
+    {
+        i++;
+    }
+    EQ(i, 0);
+    EQ(rc, FAR_PARSEERROR);
+    EQ(far.error, "Parse error: unexpected token: line 4");
+    far_clear_error(&far);
+    EQ(far.error, "");
 
     fclose(fd);
 }
